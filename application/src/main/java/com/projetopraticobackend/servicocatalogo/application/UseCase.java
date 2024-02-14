ab56a4855493eac38ed38019c9feb589ae4b625c
Rafiere <rafapgbr@gmail.com>
2024-02-14 10:36:37 -0300

    feat: add application module and tests

---
+++ application/src/main/java/com/projetopraticobackend/servicocatalogo/application/UseCase.java
@@ -0,0 +1,12 @@
+package com.projetopraticobackend.servicocatalogo.application;
+
+
+import com.projetopraticobackend.servicocatalogo.domain.Category;
+
+public class UseCase {
+
+    public Category execute(){
+
+        return new Category();
+    }
+}
