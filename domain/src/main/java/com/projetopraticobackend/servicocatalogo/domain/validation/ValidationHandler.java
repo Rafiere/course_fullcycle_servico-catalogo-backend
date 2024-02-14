57aa37abec983f97507f41e0c5f366637339ed05
Rafiere <rafapgbr@gmail.com>
2024-02-14 14:06:06 -0300

    feat: add ValidationHandler interface for all ValidationHandler implementations along the application

---
+++ domain/src/main/java/com/projetopraticobackend/servicocatalogo/domain/validation/ValidationHandler.java
@@ -0,0 +1,31 @@
+package com.projetopraticobackend.servicocatalogo.domain.validation;
+
+import java.util.List;
+
+/* Essa é uma interface fluente. Podemos chamar um método que retorna a própria
+* instância e encadear métodos. */
+public interface ValidationHandler {
+
+    /* Esse método fará o "append()" de um erro existente. */
+    ValidationHandler append(Error error);
+
+    /* Poderemos receber outros erros de um outro handler e incrementar no handler
+    * atual, como se fosse um "addAll()" de erros. */
+    ValidationHandler append(ValidationHandler handler);
+
+    /* Esse método permitirá lançarmos uma exception. */
+    ValidationHandler validate(Validation validation);
+
+    default boolean hasError(){
+        return getErrors() != null && !(getErrors().isEmpty());
+    }
+
+    /* Esse método retornará todos os erros do Handler. */
+    List<Error> getErrors();
+
+    /* Essa interface permitirá passarmos algum método que lance alguma
+    * exception. */
+    public interface Validation {
+        void validate();
+    }
+}
