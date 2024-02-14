9266e7d72da953a0de48127246deaa39c9b66e26
Rafiere <rafapgbr@gmail.com>
2024-02-14 13:05:11 -0300

    feat: add ValueObject abstract class

---
+++ domain/src/main/java/com/projetopraticobackend/servicocatalogo/domain/ValueObject.java
@@ -0,0 +1,12 @@
+package com.projetopraticobackend.servicocatalogo.domain;
+
+/* São classes abstratas ao invés de interfaces pois, se assim o fossem, algum modelo poderia
+* implementar um "ValueObject" e uma "Entity", por exemplo, o que não é algo correto. Esse foi um
+* dos motivos de ser escolhida a utilização de classes abstratas. */
+
+/* Um objeto de valor é algo definido pelos seus atributos. */
+
+public abstract class ValueObject {
+
+
+}
