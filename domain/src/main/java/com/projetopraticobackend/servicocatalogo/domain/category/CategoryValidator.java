7a225e897b7b1bf939095c5c27cecab1d300afd2
Rafiere <rafapgbr@gmail.com>
2024-02-25 09:51:43 -0300

    feat: add more validations

--- domain/src/main/java/com/projetopraticobackend/servicocatalogo/domain/category/CategoryValidator.java
+++ domain/src/main/java/com/projetopraticobackend/servicocatalogo/domain/category/CategoryValidator.java
@@ -21,8 +21,28 @@ public class CategoryValidator extends Validator {
     @Override
     public void validate() {
 
-        if(this.category.getName() == null){
+        checkNameConstraints();
+    }
+
+    private void checkNameConstraints() {
+
+        final var name = this.category.getName();
+
+        if(name == null){
             this.validationHandler().append(new Error("'name' should not be null"));
+            return;
+        }
+
+        if(name.isBlank()){
+            this.validationHandler().append(new Error("'name' should not be empty"));
+            return;
+        }
+
+        final int nameLength = name.trim().length();
+        final int MAX_NAME_LENGTH = 255;
+        final int MIN_NAME_LENGTH = 3;
+        if(nameLength < MIN_NAME_LENGTH || nameLength > MAX_NAME_LENGTH){
+            this.validationHandler().append(new Error("'name' must be between 3 and 255 characters"));
         }
     }
 }
