80ac050dbd7ad6ec1fb95b8441a4f4df2d17ca82
Rafiere <rafapgbr@gmail.com>
2024-02-25 09:51:34 -0300

    feat: add more validation unit tests for CategoryValidator

--- domain/src/test/java/com/projetopraticobackend/servicocatalogo/domain/category/CategoryTest.java
+++ domain/src/test/java/com/projetopraticobackend/servicocatalogo/domain/category/CategoryTest.java
@@ -38,15 +38,130 @@ public class CategoryTest {
         final var expectedDescription = "A categoria mais assistida";
         final var expectedIsActive = true;
 
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be null";
+
         final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
 
         /* Esperamos receber uma "DomainException" pois o nome não pode ser nulo. */
         final var actualException = Assertions.assertThrows(DomainException.class, () -> actualCategory.validate(new ThrowsValidationHandler()));
 
-        final var expectedErrorMessage = "'name' should not be null";
-
-        final var expectedErrorCount = 1;
         Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
         Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+    /* O teste abaixo fará o teste utilizando o "ThrowsValidationHandler", ao invés do "NotificationHandler" ou
+     * seja, se um erro ocorrer, ele será lançado imediatamente ao invés de acumular. */
+    @Test
+    public void givenAInvalidEmptyName_whenCallNewCategoryAndValidate_thenShouldReceiveError(){
+
+        final String expectedName = "  ";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be empty";
+
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        /* Esperamos receber uma "DomainException" pois o nome não pode ser vazio. */
+        final var actualException = Assertions.assertThrows(DomainException.class, () -> actualCategory.validate(new ThrowsValidationHandler()));
+
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+    /* O teste abaixo fará o teste utilizando o "ThrowsValidationHandler", ao invés do "NotificationHandler" ou
+     * seja, se um erro ocorrer, ele será lançado imediatamente ao invés de acumular. */
+    @Test
+    public void givenAInvalidNameLengthLessThan3_whenCallNewCategoryAndValidate_thenShouldReceiveError(){
+
+        final String expectedName = "Fi ";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' must be between 3 and 255 characters";
+
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        /* Esperamos receber uma "DomainException" pois o nome não pode ser menor do que 3 caracteres. */
+        final var actualException = Assertions.assertThrows(DomainException.class, () -> actualCategory.validate(new ThrowsValidationHandler()));
+
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+    /* O teste abaixo fará o teste utilizando o "ThrowsValidationHandler", ao invés do "NotificationHandler" ou
+     * seja, se um erro ocorrer, ele será lançado imediatamente ao invés de acumular. */
+    @Test
+    public void givenAInvalidNameLengthMoreThan255_whenCallNewCategoryAndValidate_thenShouldReceiveError(){
+
+        final String expectedName =
+                """
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In risus nibh, semper 
+                at porta tristique, laoreet in nunc. Maecenas iaculis massa id nisl consectetur ornare. Praesent 
+                condimentum, felis a varius lacinia, mi nibh feugiat odio, id fringilla libero quam sed sapien. 
+                Aenean hendrerit odio vel dui hendrerit, sit amet tempor nisl aliquet.
+                """;
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' must be between 3 and 255 characters";
+
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        /* Esperamos receber uma "DomainException" pois o nome não pode ser maior do que 255 caracteres. */
+        final var actualException = Assertions.assertThrows(DomainException.class, () -> actualCategory.validate(new ThrowsValidationHandler()));
+
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+    }
+
+    /* O teste abaixo fará o teste utilizando o "ThrowsValidationHandler", ao invés do "NotificationHandler" ou
+     * seja, se um erro ocorrer, ele será lançado imediatamente ao invés de acumular. */
+    @Test
+    public void givenAValidEmptyDescription_whenCallNewCategoryAndValidate_thenInstantiateACategory(){
+
+        final var expectedName = "Filmes";
+        final var expectedDescription = "";
+        final var expectedIsActive = true;
+
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        Assertions.assertDoesNotThrow(() -> actualCategory.validate(new ThrowsValidationHandler()));
+
+        Assertions.assertNotNull(actualCategory);
+        Assertions.assertNotNull(actualCategory.getId());
+        Assertions.assertEquals(expectedName, actualCategory.getName());
+        Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
+
+        Assertions.assertNotNull(actualCategory.getCreatedAt());
+        Assertions.assertNotNull(actualCategory.getUpdatedAt());
+        Assertions.assertNull(actualCategory.getDeletedAt());
+    }
+
+    @Test
+    public void givenAValidFalseIsActive_whenCallNewCategoryAndValidate_thenInstantiateACategory(){
+
+        final var expectedName = "Filmes";
+        final var expectedDescription = "";
+        final var expectedIsActive = false;
+
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        Assertions.assertDoesNotThrow(() -> actualCategory.validate(new ThrowsValidationHandler()));
+
+        Assertions.assertNotNull(actualCategory);
+        Assertions.assertNotNull(actualCategory.getId());
+        Assertions.assertEquals(expectedName, actualCategory.getName());
+        Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
+
+        Assertions.assertNotNull(actualCategory.getCreatedAt());
+        Assertions.assertNotNull(actualCategory.getUpdatedAt());
+        Assertions.assertNotNull(actualCategory.getDeletedAt());
     }
 }
