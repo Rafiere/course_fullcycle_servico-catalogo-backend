a39ec842f736fcef7b4684eb195b8ac3c0c90e1d
Rafiere <rafapgbr@gmail.com>
2024-03-10 20:27:54 -0300

    feat: add helper method to return first error of a handler if it exists

--- domain/src/main/java/com/projetopraticobackend/servicocatalogo/domain/validation/ValidationHandler.java
+++ domain/src/main/java/com/projetopraticobackend/servicocatalogo/domain/validation/ValidationHandler.java
@@ -20,6 +20,14 @@ public interface ValidationHandler {
         return getErrors() != null && !(getErrors().isEmpty());
     }
 
+    default Error firstError(){
+        if(getErrors() != null && !getErrors().isEmpty()){
+            return getErrors().get(0);
+        } else {
+            return null;
+        }
+    }
+
     /* Esse método retornará todos os erros do Handler. */
     List<Error> getErrors();
 
