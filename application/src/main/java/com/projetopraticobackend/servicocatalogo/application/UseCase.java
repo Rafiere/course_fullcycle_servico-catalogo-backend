bd1abeb57d8498c817dcfdd7dfd2c6a23ad1b221
Rafiere <rafapgbr@gmail.com>
2024-03-10 19:47:59 -0300

    feat: add in and out usecase type

--- application/src/main/java/com/projetopraticobackend/servicocatalogo/application/UseCase.java
+++ application/src/main/java/com/projetopraticobackend/servicocatalogo/application/UseCase.java
@@ -3,10 +3,11 @@ package com.projetopraticobackend.servicocatalogo.application;
 
 import com.projetopraticobackend.servicocatalogo.domain.category.Category;
 
-public class UseCase {
+/* Por padrão, o caso de uso receberá algo e retornará algo. Nem sempre isso
+* acontecerá, porém, esse caso de uso representará esse tipo. */
+public abstract class UseCase<IN, OUT> {
 
-    public Category execute(){
-
-        return new Category();
-    }
+    /* Por padrão, os casos de uso implementam o padrão "command()". Na
+    * literatura, eles têm um único método público, que é chamado de "execute()". */
+    public abstract OUT execute(IN in);
 }
