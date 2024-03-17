da1386278e3cd4f2c0f63ca63081a07a840ac74a
Rafiere <rafapgbr@gmail.com>
2024-03-17 19:01:15 -0300

    feat: add method to map an entity to a Pagination result

--- domain/src/main/java/com/projetopraticobackend/servicocatalogo/domain/pagination/Pagination.java
+++ domain/src/main/java/com/projetopraticobackend/servicocatalogo/domain/pagination/Pagination.java
@@ -1,6 +1,7 @@
 package com.projetopraticobackend.servicocatalogo.domain.pagination;
 
 import java.util.List;
+import java.util.function.Function;
 
 /* Esse "Pagination" poderá ser usado para qualquer entidade, como um "Category", um
  * "Genre" e etc. */
@@ -9,4 +10,14 @@ public record Pagination<T>(int currentPage, //É a página atual em que a pagin
                             int totalElements, //Quantas informações temos no banco de dados.
                             List<T> elements //Os itens que serão retornados na página atual.
 ) {
+
+    /* O "R" é o tipo do resultado, e o "T" é o tipo que ele tem. */
+    public <R> Pagination<R> map(final Function<T, R> mapper) {
+
+        final List<R> newList = this.elements.stream()
+                .map(mapper)
+                .toList();
+
+        return new Pagination<>(currentPage(), perPage(), totalElements(), newList);
+    }
 }
