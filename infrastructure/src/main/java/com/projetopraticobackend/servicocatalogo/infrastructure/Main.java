ee25ef1ed9d8c4a7d7e9f6bb63838d7b4ff502e5
Rafiere <rafapgbr@gmail.com>
2024-03-21 23:44:55 -0300

    feat: add application runner to test Flyway scripts

--- infrastructure/src/main/java/com/projetopraticobackend/servicocatalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/projetopraticobackend/servicocatalogo/infrastructure/Main.java
@@ -1,19 +1,39 @@
 package com.projetopraticobackend.servicocatalogo.infrastructure;
 
+import com.projetopraticobackend.servicocatalogo.domain.category.Category;
+import com.projetopraticobackend.servicocatalogo.infrastructure.category.persistence.CategoryJpaEntity;
+import com.projetopraticobackend.servicocatalogo.infrastructure.category.persistence.CategoryRepository;
 import com.projetopraticobackend.servicocatalogo.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.AbstractEnvironment;
+
+import java.util.List;
 
 @SpringBootApplication //Essa é a anotação que faz com que o Spring Boot saiba que essa é a classe principal da aplicação, além de configurar o Spring Boot com várias configurações iniciais.
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Iniciando a aplicação...");
-//        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development"); //Estamos definindo o perfil de execução da aplicação. Nesse caso, estamos definindo que a aplicação será executada em ambiente de desenvolvimento.
-        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "test"); //Estamos definindo o perfil de execução da aplicação. Nesse caso, estamos definindo que a aplicação será executada em ambiente de desenvolvimento.
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development"); //Estamos definindo o perfil de execução da aplicação. Nesse caso, estamos definindo que a aplicação será executada em ambiente de desenvolvimento.
+//        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "test"); //Estamos definindo o perfil de execução da aplicação. Nesse caso, estamos definindo que a aplicação será executada em ambiente de desenvolvimento.
 
         //A "WebServerConfig" é a classe que contém as configurações do servidor web que o Spring subirá.
         SpringApplication.run(WebServerConfig.class, args);
+    }
+
+    /* O código abaixo será executado depois que o contexto do Spring subir, e o Spring nos dará uma
+    * instância do "CategoryRepository". */
+    @Bean
+    public ApplicationRunner runner(CategoryRepository repository){
+        return args -> {
+            List<CategoryJpaEntity> all = repository.findAll();
+
+            Category filmes = Category.newCategory("Filmes", "Filmes de todos os gêneros.", true);
+
+            repository.saveAndFlush(CategoryJpaEntity.from(filmes));
+        };
     }
 }
