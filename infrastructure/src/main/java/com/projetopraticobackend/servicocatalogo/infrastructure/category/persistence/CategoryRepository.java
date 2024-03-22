2cd3015dacc108ded1b7b6fb6ca8edaad73fe27a
Rafiere <rafapgbr@gmail.com>
2024-03-21 23:44:33 -0300

    feat: add JPA repository for CategoryJpaEntity

---
+++ infrastructure/src/main/java/com/projetopraticobackend/servicocatalogo/infrastructure/category/persistence/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.projetopraticobackend.servicocatalogo.infrastructure.category.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<CategoryJpaEntity, String> {
+
+
+}
