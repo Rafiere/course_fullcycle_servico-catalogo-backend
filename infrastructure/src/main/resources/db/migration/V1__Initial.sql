0b2369450e873c309a7ecf0ba8a050e84324b6ed
Rafiere <rafapgbr@gmail.com>
2024-03-21 23:45:32 -0300

    feat: add category entity flyway insert sql script

--- infrastructure/src/main/resources/db/migration/V1__Initial.sql
+++ infrastructure/src/main/resources/db/migration/V1__Initial.sql
@@ -1,3 +1,9 @@
 CREATE TABLE category (
-    id BINARY(16) PRIMARY KEY
+    id VARCHAR(36) NOT NULL PRIMARY KEY, # 36 é a quantidade de caracteres do UUID. O "PK" já informa pro MySQL que essa é uma chave primária e que o MySQL precisa criar um índice, portanto, não precisamos criá-lo de forma redundante.
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(4000),
+    active BOOLEAN NOT NULL DEFAULT TRUE,
+    created_at DATETIME(6) NOT NULL,
+    updated_at DATETIME(6) NOT NULL,
+    deleted_at DATETIME(6) NULL
 )
