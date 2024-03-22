a784b474e93ddbb9afe0a2437e193e0551d68fb9
Rafiere <rafapgbr@gmail.com>
2024-03-21 22:13:03 -0300

    feat: add "test" environment as main

--- infrastructure/src/main/java/com/projetopraticobackend/servicocatalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/projetopraticobackend/servicocatalogo/infrastructure/Main.java
@@ -10,7 +10,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Iniciando a aplicação...");
-        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development"); //Estamos definindo o perfil de execução da aplicação. Nesse caso, estamos definindo que a aplicação será executada em ambiente de desenvolvimento.
+//        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development"); //Estamos definindo o perfil de execução da aplicação. Nesse caso, estamos definindo que a aplicação será executada em ambiente de desenvolvimento.
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "test"); //Estamos definindo o perfil de execução da aplicação. Nesse caso, estamos definindo que a aplicação será executada em ambiente de desenvolvimento.
 
         //A "WebServerConfig" é a classe que contém as configurações do servidor web que o Spring subirá.
         SpringApplication.run(WebServerConfig.class, args);
