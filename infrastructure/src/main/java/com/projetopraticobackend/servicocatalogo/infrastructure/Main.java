7a9e2d21f6c8fae8b381c784c0452ba5390183eb
Rafiere <rafapgbr@gmail.com>
2024-03-22 22:34:03 -0300

    fix: remove hardcoded environment

--- infrastructure/src/main/java/com/projetopraticobackend/servicocatalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/projetopraticobackend/servicocatalogo/infrastructure/Main.java
@@ -17,7 +17,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Iniciando a aplicação...");
-        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development"); //Estamos definindo o perfil de execução da aplicação. Nesse caso, estamos definindo que a aplicação será executada em ambiente de desenvolvimento.
+//        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development"); //Estamos definindo o perfil de execução da aplicação. Nesse caso, estamos definindo que a aplicação será executada em ambiente de desenvolvimento.
 //        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "test"); //Estamos definindo o perfil de execução da aplicação. Nesse caso, estamos definindo que a aplicação será executada em ambiente de desenvolvimento.
 
         //A "WebServerConfig" é a classe que contém as configurações do servidor web que o Spring subirá.
@@ -34,6 +34,8 @@ public class Main {
             Category filmes = Category.newCategory("Filmes", "Filmes de todos os gêneros.", true);
 
             repository.saveAndFlush(CategoryJpaEntity.from(filmes));
+
+            repository.deleteAll();
         };
     }
 }
