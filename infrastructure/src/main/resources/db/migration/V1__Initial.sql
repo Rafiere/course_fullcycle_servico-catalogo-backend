29aef66b6715c34b3cdca6a5046ae43b9d4bddbc
Rafiere <rafapgbr@gmail.com>
2024-03-22 22:32:46 -0300

    fix: remove comments

--- infrastructure/src/main/resources/db/migration/V1__Initial.sql
+++ infrastructure/src/main/resources/db/migration/V1__Initial.sql
@@ -1,9 +1,9 @@
 CREATE TABLE category (
-    id VARCHAR(36) NOT NULL PRIMARY KEY, # 36 é a quantidade de caracteres do UUID. O "PK" já informa pro MySQL que essa é uma chave primária e que o MySQL precisa criar um índice, portanto, não precisamos criá-lo de forma redundante.
+    id VARCHAR(36) NOT NULL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     description VARCHAR(4000),
     active BOOLEAN NOT NULL DEFAULT TRUE,
     created_at DATETIME(6) NOT NULL,
     updated_at DATETIME(6) NOT NULL,
     deleted_at DATETIME(6) NULL
-)
+);
