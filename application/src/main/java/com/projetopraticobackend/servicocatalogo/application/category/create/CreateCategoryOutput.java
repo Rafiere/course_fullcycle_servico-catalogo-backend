79c12c9cdfa4b09e70f28e1ddee20adb4a727643
Rafiere <rafapgbr@gmail.com>
2024-04-05 09:47:44 -0300

    feat: add from method to create a Category only with an ID

--- application/src/main/java/com/projetopraticobackend/servicocatalogo/application/category/create/CreateCategoryOutput.java
+++ application/src/main/java/com/projetopraticobackend/servicocatalogo/application/category/create/CreateCategoryOutput.java
@@ -1,5 +1,6 @@
 package com.projetopraticobackend.servicocatalogo.application.category.create;
 
+import com.projetopraticobackend.servicocatalogo.application.category.retrieve.get.CategoryOutput;
 import com.projetopraticobackend.servicocatalogo.domain.category.Category;
 import com.projetopraticobackend.servicocatalogo.domain.category.CategoryID;
 
@@ -8,7 +9,11 @@ public record CreateCategoryOutput(
         CategoryID id
 ) {
 
-    public static CreateCategoryOutput from(Category category) {
+    public static CreateCategoryOutput from(final Category category) {
         return new CreateCategoryOutput(category.getId());
+    }
+
+    public static CreateCategoryOutput from(final CategoryID id){
+        return new CreateCategoryOutput(id);
     }
 }
