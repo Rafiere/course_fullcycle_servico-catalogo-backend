c097dcb6454964a3ddbef57aa10bb6bb29b2061a
Rafiere <rafapgbr@gmail.com>
2024-04-05 08:15:27 -0300

    feat: inject first usecase

--- infrastructure/src/main/java/com/projetopraticobackend/servicocatalogo/infrastructure/category/api/controllers/CategoryController.java
+++ infrastructure/src/main/java/com/projetopraticobackend/servicocatalogo/infrastructure/category/api/controllers/CategoryController.java
@@ -1,12 +1,23 @@
 package com.projetopraticobackend.servicocatalogo.infrastructure.category.api.controllers;
 
+import com.projetopraticobackend.servicocatalogo.application.category.create.CreateCategoryUseCase;
 import com.projetopraticobackend.servicocatalogo.domain.pagination.Pagination;
 import com.projetopraticobackend.servicocatalogo.infrastructure.category.api.CategoryAPI;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @RestController
 public class CategoryController implements CategoryAPI {
+
+    //Injetaremos os casos de uso que serão chamados pelo controller.
+    private final CreateCategoryUseCase createCategoryUseCase;
+
+    public CategoryController(final CreateCategoryUseCase createCategoryUseCase) {
+        this.createCategoryUseCase = Objects.requireNonNull(createCategoryUseCase);
+    }
+
     @Override
     public ResponseEntity<?> createCategory() {
         return null;
