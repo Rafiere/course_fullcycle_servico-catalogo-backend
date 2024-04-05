dae265ec57d8e14bb2ac4e80df0933f53e22b53a
Rafiere <rafapgbr@gmail.com>
2024-04-05 08:15:43 -0300

    fix: remove "*" to avoid scan errors

--- infrastructure/src/test/java/com/projetopraticobackend/servicocatalogo/MySQLGatewayTest.java
+++ infrastructure/src/test/java/com/projetopraticobackend/servicocatalogo/MySQLGatewayTest.java
@@ -19,7 +19,7 @@ import java.util.Collection;
 @ActiveProfiles("test") //Estamos definindo que utilizaremos o perfil "test" ao executarmos os testes.
 //Essa anotação será utilizada pois ela não sobe o contexto inteiro do Spring. Ela apenas configura o contexto necessário para testarmos o repositório. O problema de usar essa anotação é que ele não enxerga o gateway que está anotado com o "@Service". Para fazer ele escanear, precisamos utilizar a anotação "@ComponentScan".
 @ComponentScan(includeFilters = { //Com essa anotação, diremos para o Spring quem devemos escanear além do que foi escaneado pelo "DataJpaTest". Essa configuração será feita através de filtros.
-        @ComponentScan.Filter(type = FilterType.REGEX, pattern = ".*[MySQLGateway]") //Esse filtro será feito através de "RegEx". Assim, o Spring escaneará, além de tudo o que foi configurado pelo "@DataJpaTest", todas as classes que terminarem com "MySQLGateway" para esses testes.
+        @ComponentScan.Filter(type = FilterType.REGEX, pattern = ".[MySQLGateway]") //Esse filtro será feito através de "RegEx". Assim, o Spring escaneará, além de tudo o que foi configurado pelo "@DataJpaTest", todas as classes que terminarem com "MySQLGateway" para esses testes.
 })
 //@SpringBootTest //Essa anotação faz com que o Spring Boot suba o contexto da aplicação antes de rodar os testes. Esse contexto é recomendado apenas para testes end-to-end. Por isso, não utilizaremos essa anotação.
 @DataJpaTest //Essa anotação faz com que o Spring Boot suba o contexto do JPA antes de rodar os testes. Esse contexto é recomendado para testes de integração. Por isso, utilizaremos essa anotação.
