947c9a32895b412064c12e5aead0b080ff76083c
Rafiere <rafapgbr@gmail.com>
2024-04-05 07:39:21 -0300

    feat: add initial documentation to CategoryAPI

--- infrastructure/src/main/java/com/projetopraticobackend/servicocatalogo/infrastructure/category/api/CategoryAPI.java
+++ infrastructure/src/main/java/com/projetopraticobackend/servicocatalogo/infrastructure/category/api/CategoryAPI.java
@@ -4,6 +4,10 @@ package com.projetopraticobackend.servicocatalogo.infrastructure.category.api;
  * exposição. */
 
 import com.projetopraticobackend.servicocatalogo.domain.pagination.Pagination;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,13 +17,26 @@ import org.springframework.web.bind.annotation.*;
 * a classe "Controller" original. */
 
 @RequestMapping("/categories")
+@Tag(name = "Categories") //É o nome do "resource" que estamos expondo.
 public interface CategoryAPI {
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
                  produces = MediaType.APPLICATION_JSON_VALUE)
+    @Operation(summary = "Create a new category") //Aqui temos uma descrição do que o endpoint faz.
+    @ApiResponses(value = { //Aqui temos as possíveis respostas desse endpoint.
+            @ApiResponse(responseCode = "201", description = "Created successfully"),
+            @ApiResponse(responseCode = "422", description = "A validation error was thrown"),
+            @ApiResponse(responseCode = "500", description = "An internal server error was thrown")
+    })
     ResponseEntity<?> createCategory();
 
     @GetMapping
+    @Operation(summary = "List all categories paginated")
+    @ApiResponses(value = { //Aqui temos as possíveis respostas desse endpoint.
+            @ApiResponse(responseCode = "200", description = "Listed successfully"),
+            @ApiResponse(responseCode = "422", description = "An invalid parameter was received"),
+            @ApiResponse(responseCode = "500", description = "An internal server error was thrown")
+    })
     Pagination<?> listCategories(@RequestParam(name = "search", required = false, defaultValue = "") final String name,
                                  @RequestParam(name = "page", required = false, defaultValue = "0") final Integer page, //Se o usuário não enviar nenhuma página padrão, ela será "0".
                                  @RequestParam(name = "perPage", required = false, defaultValue = "10") final Integer perPage, //O Spring fará a conversão do "defaultValue" automaticamente, se necessário.
