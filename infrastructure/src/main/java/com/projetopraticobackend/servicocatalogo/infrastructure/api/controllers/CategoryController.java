ad8013f0429ee1e8d99b3a2c5df0039c27733430
Rafiere <rafapgbr@gmail.com>
2024-04-07 11:51:05 -0300

    feat: add integration tests for category web controllers and web controllers

--- infrastructure/src/main/java/com/projetopraticobackend/servicocatalogo/infrastructure/api/controllers/CategoryController.java
+++ infrastructure/src/main/java/com/projetopraticobackend/servicocatalogo/infrastructure/api/controllers/CategoryController.java
@@ -1,12 +1,18 @@
 package com.projetopraticobackend.servicocatalogo.infrastructure.api.controllers;
 
+import com.projetopraticobackend.servicocatalogo.application.category.create.CreateCategoryCommand;
+import com.projetopraticobackend.servicocatalogo.application.category.create.CreateCategoryOutput;
 import com.projetopraticobackend.servicocatalogo.application.category.create.CreateCategoryUseCase;
 import com.projetopraticobackend.servicocatalogo.domain.pagination.Pagination;
+import com.projetopraticobackend.servicocatalogo.domain.validation.handler.Notification;
 import com.projetopraticobackend.servicocatalogo.infrastructure.api.CategoryAPI;
+import com.projetopraticobackend.servicocatalogo.infrastructure.category.models.CreateCategoryApiInput;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
 import java.util.Objects;
+import java.util.function.Function;
 
 @RestController
 public class CategoryController implements CategoryAPI {
@@ -19,8 +25,27 @@ public class CategoryController implements CategoryAPI {
     }
 
     @Override
-    public ResponseEntity<?> createCategory() {
-        return null;
+    public ResponseEntity<?> createCategory(final CreateCategoryApiInput createCategoryApiInput) {
+
+        final var command = CreateCategoryCommand.with(
+                createCategoryApiInput.name(),
+                createCategoryApiInput.description(),
+                createCategoryApiInput.active() != null ? createCategoryApiInput.active() : true);
+
+
+        /* No "Fold", o "Left" é o objeto que tem o "Notification", que é quando algo deu errado
+        * na API, e o "Right" é o output. */
+
+        /* Se um erro acontecer, será enviado o "notification" com o status 422. */
+        final Function<Notification, ResponseEntity<?>> onError =
+            notification -> ResponseEntity.unprocessableEntity().body(notification);
+
+        /* Se tudo ocorrer bem, será enviado o "output" com o status 201 e o cabeçalho "Location". */
+        final Function<CreateCategoryOutput, ResponseEntity<?>> onSuccess =
+            output -> ResponseEntity.created(URI.create("/categories/" + output.id())).body(output);
+
+        return createCategoryUseCase.execute(command)
+                .fold(onError, onSuccess);
     }
 
     @Override
--- infrastructure/src/test/java/com/projetopraticobackend/servicocatalogo/infrastructure/api/CategoryAPITest.java
+++ infrastructure/src/test/java/com/projetopraticobackend/servicocatalogo/infrastructure/api/CategoryAPITest.java
@@ -3,22 +3,25 @@ package com.projetopraticobackend.servicocatalogo.infrastructure.api;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.projetopraticobackend.servicocatalogo.ControllerTest;
-import com.projetopraticobackend.servicocatalogo.application.category.create.CreateCategoryCommand;
 import com.projetopraticobackend.servicocatalogo.application.category.create.CreateCategoryOutput;
 import com.projetopraticobackend.servicocatalogo.application.category.create.CreateCategoryUseCase;
-import com.projetopraticobackend.servicocatalogo.application.category.retrieve.get.CategoryOutput;
-import com.projetopraticobackend.servicocatalogo.domain.category.CategoryID;
+import com.projetopraticobackend.servicocatalogo.domain.exceptions.DomainException;
+import com.projetopraticobackend.servicocatalogo.domain.validation.Error;
+import com.projetopraticobackend.servicocatalogo.domain.validation.handler.Notification;
 import com.projetopraticobackend.servicocatalogo.infrastructure.category.models.CreateCategoryApiInput;
 import io.vavr.API;
-import io.vavr.control.Either;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Objects;
 
 /* Nessa classe, teremos os testes integrados dos controllers de criação de categoria. */
 
@@ -51,23 +54,23 @@ public class CategoryAPITest {
         //Estamos criando a implementação do mock do UseCase, pois não o testaremos, e sim faremos o teste integrado
         //do controller. Quando esse caso de uso for chamado, ele retornará uma categoria com o ID "123".
         Mockito.when(createCategoryUseCase.execute(Mockito.any()))
-                .thenReturn(API.Right(CreateCategoryOutput.from(CategoryID.from("123"))));
+                .thenReturn(API.Right(CreateCategoryOutput.from("123")));
 
         //Vamos utilizar o "mockMvc" para simularmos um "POST" para o controller.
 
         //Estamos fazendo um "POST" para a URL "/categories" passando o "command" como JSON.
         final var request = MockMvcRequestBuilders.post("/categories")
-                .contentType("application/json")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .content(objectMapper.writeValueAsString(input)) //É o JSON que estamos enviando.
                 .accept("application/json");
 
         //Estamos realizando uma requisição HTTP real para o controller e validando o status retornado e o header "Location".
         mockMvc.perform(request)
-                .andDo(MockMvcResultHandlers.print())
-                .andExpectAll( //Aqui passaremos uma lista de predicados para validarmos a resposta.
-                        MockMvcResultMatchers.status().isCreated(),
-                        MockMvcResultMatchers.header().string("Location", "/categories/123")
-                );
+                .andDo(MockMvcResultHandlers.print()) //Aqui temos vários predicados para validarmos as respostas.
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.header().string("Location", "/categories/123"))
+                .andExpect(MockMvcResultMatchers.header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id", Matchers.equalTo("123"))); //Estamos validando se o campo "id" do JSON retornado é "123".
 
         /* Estamos garantindo que o caso de uso foi chamado apenas uma única vez e que o argumento que foi passado para o
         * caso de uso está correto, ou seja, que o controller converteu corretamente a request em um command para o
@@ -75,6 +78,83 @@ public class CategoryAPITest {
         Mockito.verify(createCategoryUseCase, Mockito.times(1)).execute(Mockito.argThat(
                 command -> command.name().equals(expectedName) &&
                            command.description().equals(expectedDescription) &&
+                           command.isActive() == expectedIsActive
+        ));
+    }
+
+    //Abaixo, temos o cenário triste.
+    @Test
+    public void givenAInvalidName_whenCallsCreateCategory_thenShouldReturnNotification() throws Exception {
+
+        final String expectedName = null;
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var expectedErrorMessage = "'name' should not be null";
+
+        final var input = new CreateCategoryApiInput(expectedName, expectedDescription, expectedIsActive);
+
+        //Estamos criando a implementação do mock do UseCase, pois não o testaremos, e sim faremos o teste integrado
+        //do controller. Quando esse caso de uso for chamado, ele retornará um erro.
+        Mockito.when(createCategoryUseCase.execute(Mockito.any()))
+                .thenReturn(API.Left(Notification.create(new Error(expectedErrorMessage))));
+
+        //Vamos utilizar o "mockMvc" para simularmos um "POST" para o controller.
+
+        //Estamos fazendo um "POST" para a URL "/categories" passando o "command" como JSON.
+        final var request = MockMvcRequestBuilders.post("/categories")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(input)) //É o JSON que estamos enviando.
+                .accept("application/json");
+
+        //Estamos realizando uma requisição HTTP real para o controller e validando o status retornado e o header "Location".
+        mockMvc.perform(request)
+                .andDo(MockMvcResultHandlers.print()) //Aqui temos vários predicados para validarmos as respostas.
+                .andExpect(MockMvcResultMatchers.status().isUnprocessableEntity())
+                .andExpect(MockMvcResultMatchers.header().string("Location", Matchers.nullValue()))
+                .andExpect(MockMvcResultMatchers.header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.errors", Matchers.hasSize(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.errors[0].message", Matchers.equalTo(expectedErrorMessage))); //Estamos validando se o campo "message" do JSON retornado é uma mensagem de erro.
+
+        /* Estamos garantindo que o caso de uso foi chamado apenas uma única vez e que o argumento que foi passado para o
+         * caso de uso está correto, ou seja, que o controller converteu corretamente a request em um command para o
+         * caso de uso. */
+        Mockito.verify(createCategoryUseCase, Mockito.times(1)).execute(Mockito.argThat(
+                command -> Objects.equals(command.name(), expectedName) &&
+                           Objects.equals(command.description(), expectedDescription) &&
+                           command.isActive() == expectedIsActive
+        ));
+    }
+
+    @Test
+    public void givenAInvalidCommand_whenCallsCreateCategory_thenShouldReturnDomainException() throws Exception {
+
+        final String expectedName = null;
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var expectedErrorMessage = "'name' should not be null";
+
+        final var input = new CreateCategoryApiInput(expectedName, expectedDescription, expectedIsActive);
+
+        Mockito.when(createCategoryUseCase.execute(Mockito.any()))
+                .thenThrow(DomainException.with(new Error(expectedErrorMessage)));
+
+        final var request = MockMvcRequestBuilders.post("/categories")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(objectMapper.writeValueAsString(input));
+
+        mockMvc.perform(request)
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isUnprocessableEntity())
+                .andExpect(MockMvcResultMatchers.header().string("Location", Matchers.nullValue()))
+                .andExpect(MockMvcResultMatchers.header().string("Content-Type", MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.errors", Matchers.hasSize(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.errors[0].message", Matchers.equalTo(expectedErrorMessage)));
+
+        Mockito.verify(createCategoryUseCase, Mockito.times(1)).execute(Mockito.argThat(
+                command -> Objects.equals(command.name(), expectedName) &&
+                           Objects.equals(command.description(), expectedDescription) &&
                            command.isActive() == expectedIsActive
         ));
     }
